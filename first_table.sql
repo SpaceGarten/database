2c3a2f41899c46a9292137e8ae895ec9695bba20
Matthew <34130768+SpaceGarten@users.noreply.github.com>
2023-01-19 11:35:39 -0800

    Update first_table.sql

--- first_table.sql
+++ first_table.sql
@@ -19,16 +19,76 @@ CREATE TABLE staff (
 DESC staff;
 
 
-
-INSERT INTO staff(first_name, 
-last_name, 
-license_no, 
-staff_type, 
-username, 
-salary, 
-hire_date
+-- Adds new data entry
+INSERT INTO staff(
+    first_name, 
+    last_name, 
+    license_no, 
+    staff_type, 
+    username, 
+    salary, 
+    hire_date
 ) 
+VALUES (
+    'Jon',
+    'Jones',
+    '302342',
+    'Dental Assistant',
+    'jjones',
+    '23423',
+    '2023/01/01'
+), (
+    'Lan',
+    'Bass',
+    '234234',
+    'Dental Assistant',
+    'lbass',
+    '100000',
+    '2023/02/01'
 
+), (
+    'Dan',
+    'Bark',
+    '6322',
+    'Dental Assistant',
+    'dbar',
+    '12345678',
+    '2023/03/01'
+
+), (
+    'Jerry',
+    'J',
+    '114141',
+    'Dental Assistant',
+    'jjrr',
+    '4000000',
+    '2023/04/01'
+
+), (
+    'Pink',
+    'P',
+    '2722135',
+    'Dental Assistant',
+    'pp',
+    '1321231',
+    '2023/05/01'
+
+);
+
+
+-- Delete data entry
+DELETE FROM staff WHERE license_no=302342 ORDER BY last_name DESC LIMIT 1;
+
+
+-- Finding data, sorting
+SELECT first_name, last_name, MAX(salary) FROM staff;
+
+SELECT first_name, last_name, salary FROM staff WHERE salary > 20000;
+
+
+
+-- sync
+source C:\Users\Matt_\OneDrive\Documents\1201\Week 3
 
 
 -- Log in to mySQL: root from application, run XAMPP/xamppfiles/bin/mysql
@@ -37,4 +97,10 @@ hire_date
 
 -- USE class4Demo;
 
--- show tables;
+-- show tables;    
+
+-- when inserting new data, INSERT INTO staff and include all the values intcluded
+
+-- select * from staff;                  shows all added entry
+
+-- to Delete a data entry, use DELETE FROM

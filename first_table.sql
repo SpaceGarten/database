071ddf0fc52b6e15e745a81bf47382e174822722
Matthew <34130768+SpaceGarten@users.noreply.github.com>
2023-01-19 10:54:17 -0800

    Update first_table.sql

--- first_table.sql
+++ first_table.sql
@@ -1,7 +1,9 @@
+DROP DATABASE IF EXISTS class4Demo;
 
-DROP DATABASE IF EXIST class4Demo
+-- re-create the database
+CREATE DATABASE class4DEMO;
 
-
+-- Create a table
 CREATE TABLE staff (
     staff_id INT AUTO_INCREMENT PRIMARY KEY, 
     first_name VARCHAR(32) NOT NULL,
@@ -12,3 +14,27 @@ CREATE TABLE staff (
     salary FLOAT(8, 2) NOT NULL,
     hire_date date NOT NULL
 );
+
+-- show the table
+DESC staff;
+
+
+
+INSERT INTO staff(first_name, 
+last_name, 
+license_no, 
+staff_type, 
+username, 
+salary, 
+hire_date
+) 
+
+
+
+-- Log in to mySQL: root from application, run XAMPP/xamppfiles/bin/mysql
+
+-- CREATE DATABASE class4DEMO;
+
+-- USE class4Demo;
+
+-- show tables;

0f2a3dfa11c440091845f3ab02aca73df499094d
Muochu Hu <34130768+SpaceGarten@users.noreply.github.com>
2023-04-04 21:11:05 -0700

    Create northwind.sql

---
+++ Northwind/northwind.sql
@@ -0,0 +1,20 @@
+  -- Select all the rows for ContactName and Contact Title in the Orders Database
+
+SELECT ContactName, ContactTitle
+FROM Customers
+WHERE CustomerID IN (
+    SELECT CustomerID
+    FROM Orders
+)
+
+
+-- Select all the products that have a UnitPrice between 10.00 and 20.00. List the ProductName, UnitPrice
+SELECT ProductName, UnitPrice FROM Products WHERE UnitPrice BETWEEN 10.00 AND 20.00;
+
+
+-- Select all the product details for products that are shipped from Germany, Italy and Norway
+SELECT * FROM Products 
+WHERE SupplierID IN 
+(SELECT SupplierID FROM Suppliers 
+WHERE Country IN ('Germany', 'Italy', 'Norway'));
+
